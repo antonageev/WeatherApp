769184d644f53b3593fd4b375f8d5d403cddc664
Anton Ageev <antforwork@mail.ru>
2020-03-14 18:51:48 +0300

    intermediate commit before switch to lesson 2 branch

--- app/src/main/java/com/antonageev/weatherapp/MainActivity.java
+++ app/src/main/java/com/antonageev/weatherapp/MainActivity.java
@@ -1,13 +1,12 @@
 package com.antonageev.weatherapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
